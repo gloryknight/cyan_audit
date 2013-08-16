9f0985d2d2bbcecabc2d4deb65e6b326ee68926f
Moshe Jacobson <moshe@neadwerx.com>
2013-08-16 19:34:24 +0000

    Now 0.3
    git-svn-id: file:///home/kirk/migrate/svn/neadwerx_corporate/audit_log@1434 f0f64773-f6e6-4063-b836-b6bb349b41fa

---
+++ auditlog--0.2--0.3.sql
@@ -0,0 +1,324 @@
+-- fn_label_audit_transaction
+CREATE OR REPLACE FUNCTION @extschema@.fn_label_audit_transaction
+(
+    in_label    varchar,
+    in_txid     bigint default txid_current()
+)
+returns bigint as
+ $_$
+declare
+    my_audit_transaction_type   integer;
+begin
+    select @extschema@.fn_get_or_create_audit_transaction_type(in_label)
+      into my_audit_transaction_type;
+
+    update @extschema@.tb_audit_event_current
+       set audit_transaction_type = my_audit_transaction_type
+     where txid = in_txid
+       and audit_transaction_type is null;
+
+    return in_txid;
+end
+ $_$
+    language 'plpgsql' strict;
+
+
+
+-- fn_drop_audit_event_log_trigger
+CREATE OR REPLACE FUNCTION @extschema@.fn_drop_audit_event_log_trigger 
+(
+    in_table_name   varchar
+)
+returns void as
+ $_$
+declare
+    my_function_name    varchar;
+begin
+    my_function_name := 'fn_log_audit_event_'||in_table_name;
+
+    set client_min_messages to warning;
+
+    perform p.proname
+       from pg_catalog.pg_depend d
+       join pg_catalog.pg_proc p
+         on d.classid = 'pg_proc'::regclass::oid
+        and d.objid = p.oid
+       join pg_catalog.pg_extension e
+         on d.refclassid = 'pg_extension'::regclass::oid
+        and d.refobjid = e.oid
+      where e.extname = 'auditlog'
+        and p.proname = my_function_name;
+
+    if found then
+        execute 'alter extension auditlog drop function '
+             || '@extschema@.' || my_function_name|| '()';
+    end if;
+
+    perform p.proname
+       from pg_proc p
+       join pg_namespace n
+         on p.pronamespace = n.oid
+        and n.nspname = 'public'
+      where p.proname = '@extschema@.my_function_name';
+
+    if found then
+        execute 'drop function '
+             || '@extschema@.'||my_function_name||'() cascade';
+    end if;
+
+    set client_min_messages to notice;
+end
+ $_$
+    language 'plpgsql';
+
+
+
+-- fn_update_audit_event_log_trigger_on_table
+CREATE OR REPLACE FUNCTION @extschema@.fn_update_audit_event_log_trigger_on_table
+(
+    in_table_name   varchar
+)
+returns void as
+ $_$
+use strict;
+
+my $table_name = $_[0];
+
+return if $table_name =~ /tb_audit_.*/;
+
+my $table_q = "select relname "
+            . "  from pg_class c "
+            . "  join pg_namespace n "
+            . "    on c.relnamespace = n.oid "
+            . " where n.nspname = 'public' "
+            . "   and c.relname = '$table_name' ";
+
+my $table_rv = spi_exec_query($table_q);
+
+if( $table_rv->{'processed'} == 0 )
+{
+    elog(NOTICE, "Cannot audit invalid table '$table_name'");
+    return;
+}
+
+my $colnames_q = "select audit_field, column_name "
+               . "  from @extschema@.tb_audit_field "
+               . " where table_name = '$table_name' "
+               . "   and active = true ";
+
+my $colnames_rv = spi_exec_query($colnames_q);
+
+if( $colnames_rv->{'processed'} == 0 )
+{
+    my $q = "select @extschema@.fn_drop_audit_event_log_trigger('$table_name')";
+    eval{ spi_exec_query($q) };
+    elog(ERROR, "fn_drop_audit_event_log_trigger: $@") if($@);
+    return;
+}
+
+my $pk_q = "select @extschema@.fn_get_table_pk_col('$table_name') as pk_col ";
+
+my $pk_rv = spi_exec_query($pk_q);
+
+my $pk_col = $pk_rv->{'rows'}[0]{'pk_col'};
+
+unless( $pk_col )
+{
+    my $pk2_q = "select column_name as pk_col "
+              . "  from @extschema@.tb_audit_field "
+              . " where table_pk = audit_field "
+              . "   and table_name = '$table_name'";
+
+    my $pk2_rv = spi_exec_query($pk2_q);
+
+    $pk_col = $pk2_rv->{'rows'}[0]{'pk_col'};
+
+    unless( $pk_col )
+    {
+        elog(NOTICE, "pk_col is null");
+        return;
+    }
+}
+
+my $fn_q = <<EOF;
+CREATE OR REPLACE FUNCTION @extschema@.fn_log_audit_event_$table_name()
+returns trigger as
+ \$_\$
+-- THIS FUNCTION AUTOMATICALLY GENERATED. DO NOT EDIT
+DECLARE
+    my_row_pk_val       integer;
+    my_old_row          record;
+    my_new_row          record;
+    my_recorded         timestamp;
+BEGIN
+    if( TG_OP = 'INSERT' ) then
+        my_row_pk_val := NEW.$pk_col;
+    else
+        my_row_pk_val := OLD.$pk_col;
+    end if;
+
+    if( TG_OP = 'DELETE' ) then
+        my_new_row := OLD;
+    else
+        my_new_row := NEW;
+    end if;
+    if( TG_OP = 'INSERT' ) then
+        my_old_row := NEW;
+    else
+        my_old_row := OLD;
+    end if;
+
+    if current_setting('@extschema@.enabled') = '0' then
+        return my_new_row;
+    end if;
+
+    perform @extschema@.fn_set_last_audit_txid();
+
+    my_recorded := clock_timestamp();
+
+EOF
+
+foreach my $row (@{$colnames_rv->{'rows'}})
+{
+    my $column_name = $row->{'column_name'};
+    my $audit_field = $row->{'audit_field'};
+
+    $fn_q .= <<EOF;
+    IF (TG_OP = 'INSERT' AND
+        my_new_row.$column_name IS NOT NULL) OR
+       (TG_OP = 'UPDATE' AND
+        my_new_row.${column_name}::text IS DISTINCT FROM
+        my_old_row.${column_name}::text) OR
+       (TG_OP = 'DELETE')
+    THEN
+        perform @extschema@.fn_new_audit_event(
+                    $audit_field,
+                    my_row_pk_val,
+                    my_recorded,
+                    TG_OP,
+                    my_old_row.$column_name,
+                    my_new_row.$column_name
+                );
+    END IF;
+
+EOF
+}
+
+$fn_q .= <<EOF;
+    return NEW;
+EXCEPTION
+    WHEN undefined_function THEN
+         raise notice 'undefined function';
+         return NEW;
+    WHEN invalid_column_reference THEN
+         raise notice 'invalid column reference';
+         return NEW;
+END
+ \$_\$
+    language 'plpgsql';
+EOF
+
+eval { spi_exec_query($fn_q) };
+elog(ERROR, $@) if $@;
+
+my $tg_q = "CREATE TRIGGER tr_log_audit_event_$table_name "
+         . "   after insert or update or delete on $table_name for each row "
+         . "   execute procedure @extschema@.fn_log_audit_event_$table_name()";
+eval { spi_exec_query($tg_q) };
+
+my $ext_q = "ALTER EXTENSION auditlog ADD FUNCTION @extschema@.fn_log_audit_event_$table_name()";
+
+eval { spi_exec_query($ext_q) };
+ $_$
+    language 'plperl';
+
+
+SELECT pg_catalog.pg_extension_config_dump('@extschema@.sq_pk_audit_data_type','');
+
+-- tb_audit_field
+create sequence @extschema@.sq_pk_audit_field;
+
+alter table @extschema@.tb_audit_field
+    alter column table_name set not null,
+    alter column column_name set not null;
+
+SELECT pg_catalog.pg_extension_config_dump('@extschema@.sq_pk_audit_field','');
+
+
+
+-- fn_audit_event_log_trigger_updater
+CREATE OR REPLACE FUNCTION @extschema@.fn_audit_event_log_trigger_updater()
+returns trigger as
+ $_$
+declare
+    my_table_name   varchar;
+begin
+    if TG_OP = 'DELETE' then
+        my_table_name := OLD.table_name;
+    else
+        my_table_name := NEW.table_name;
+    end if;
+
+    perform c.relname
+       from pg_class c
+       join pg_namespace n
+         on c.relnamespace = n.oid
+      where n.nspname = 'public'
+        and c.relname = my_table_name;
+
+    if found then
+        perform @extschema@.fn_update_audit_event_log_trigger_on_table(my_table_name);
+    end if;
+    return new;
+end
+ $_$
+    language 'plpgsql';
+
+
+-- fn_check_audit_field_validity
+CREATE OR REPLACE FUNCTION @extschema@.fn_check_audit_field_validity()
+returns trigger as
+ $_$
+declare
+    my_pk_col           varchar;
+    my_audit_data_type  integer;
+begin
+    if TG_OP = 'UPDATE' then
+        if NEW.table_name  != OLD.table_name or
+           NEW.column_name != OLD.column_name
+        then
+            raise exception 'Updating table_name or column_name not allowed.';
+        end if;
+    end if;
+
+    if NEW.active = false then
+        return NEW;
+    end if;
+
+    if( NEW.table_pk is null ) then
+        my_pk_col := @extschema@.fn_get_table_pk_col(NEW.table_name);
+
+        if my_pk_col is null then
+            raise exception 'Cannot audit table %: No PK column found',
+                NEW.table_name;
+        end if;
+
+        if my_pk_col = NEW.column_name then
+            NEW.table_pk := NEW.audit_field;
+        else
+            select @extschema@.fn_get_or_create_audit_field(NEW.table_name, my_pk_col)
+              into NEW.table_pk;
+        end if;
+    end if;
+
+    if NEW.audit_data_type is null then
+        NEW.audit_data_type := @extschema@.fn_get_or_create_audit_data_type(
+            @extschema@.fn_get_column_data_type(NEW.table_name, NEW.column_name)
+        );
+    end if;
+
+    return NEW;
+end
+ $_$
+    language plpgsql;
+
