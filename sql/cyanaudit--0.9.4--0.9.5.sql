bec30bf8b656f61b0cfbac0afbc11aff752665da
Moshe Jacobson <moshe@neadwerx.com>
2014-10-24 15:02:06 -0400

    Added upgrade for updated function

--- sql/cyanaudit--0.9.4--0.9.5.sql
+++ sql/cyanaudit--0.9.4--0.9.5.sql
@@ -47,3 +47,55 @@ end
  $_$
     language plpgsql stable strict;
 
+
+
+-- fn_get_or_create_audit_field
+CREATE OR REPLACE FUNCTION @extschema@.fn_get_or_create_audit_field
+(
+    in_table_name       varchar,
+    in_column_name      varchar,
+    in_audit_data_type  integer default null
+)
+returns integer as
+ $_$
+declare
+    my_audit_field   integer;
+    my_active        boolean;
+begin
+    select audit_field
+      into my_audit_field
+      from @extschema@.tb_audit_field
+     where table_name = in_table_name
+       and column_name = in_column_name;
+
+    -- if we do not yet know about the field
+    if not found then
+        -- set it active if another field in this table is already active
+        select active
+          into my_active
+          from @extschema@.tb_audit_field
+         where table_name = in_table_name
+         order by active desc
+         limit 1;
+
+        -- If no columns of this table are known, 
+        -- or this table is currently being logged,
+        if my_active is distinct from false then
+            -- set it active if the column is currently real in the db
+            select count(*)::integer::boolean
+              into my_active
+              from information_schema.columns
+             where table_schema = 'public'
+               and table_name = in_table_name
+               and column_name = in_column_name;
+        end if;
+
+        insert into @extschema@.tb_audit_field
+        (
+            table_name,
+            column_name,
+            active,
+            audit_data_type
+        )
+        values
+        (
